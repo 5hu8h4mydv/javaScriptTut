3ba7a904aecbfbbb828d145808a41ebfaf1f1cf3
5hu8h4mydv <132751573+5hu8h4mydv@users.noreply.github.com>
2026-02-19 13:37:40 +0000

    Arrays

---
+++ Basics/01_Arrays.js
@@ -0,0 +1,7 @@
+const myHeros = ["spiderman","IronMan","Thor"]
+const myDcHeros = ["superman","BatMan","","flash"]
+
+// myHeros.push(myDcHeros)
+const myNewArr = myHeros.concat(myDcHeros)
+
+console.log(myNewArr)
