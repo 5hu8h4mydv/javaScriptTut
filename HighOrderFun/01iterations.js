d7907e4086b38c418fefac7663108c681bef7783
5hu8h4mydv <132751573+5hu8h4mydv@users.noreply.github.com>
2026-02-24 12:30:47 +0000

    basic Complwtwd

--- HighOrderFun/01iterations.js
+++ HighOrderFun/01iterations.js
@@ -2,11 +2,6 @@ const  arr = [1,2,3,4,5]
 
 const myString = "Hello Dosto"
 
-const myObj ={
-    name:"Shubham",
-    rollno:12,
-    age:23
-}
 
 
 // for (const element of myObj) {                          //object is not iterable in for Of loop.
@@ -21,7 +16,48 @@ map.set('USA',"United States of America")
 map.set('FR',"France")
 map.set('UK',"United Kingdom")
 
-for (const [keys,values] of map) {                          //object is not iterable in for Of loop.
-    console.log(keys,values)
+// for (const [keys,values] of map) {                          //object is not iterable in for Of loop.
+//     console.log(keys,values)
     
-}
+// }
+
+// const myObj ={
+// //     name:"Shubham",
+// //     rollno:12,
+// //     age:23
+// // }
+
+
+// const newArry =["Raghav","Shubham","Nil","Joseph"]
+
+// for (const keys in newArry) {
+//    console.log(newArry[keys])
+    
+    
+// }
+
+const newObj = [
+    {
+        languageName:"java",
+        languageFileName:"java"
+
+    },
+    {
+        languageName:"javaScript",
+        languageFileName:"js"
+
+    },
+    {
+        languageName:"python",
+        languageFileName:"py"
+    },
+    {
+      languageName:"ruby",
+        languageFileName:"rb"
+    }
+]
+
+newObj.forEach(function (item){
+    console.log(item.languageFileName)
+
+});
---
+++ HighOrderFun/02arfun.js
@@ -0,0 +1,15 @@
+// const myArr = [1,2,3,4,5]
+
+//  const newArr = myArr.forEach((item)=>{
+//     return console.log(item)
+//  })
+
+//  console.log(newArr)
+
+const myArr = [12,13,54,87]
+
+const newArr =  myArr.filter((num)=>{
+    return num+1
+})
+
+console.log(newArr)
